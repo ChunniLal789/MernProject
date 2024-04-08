f897d08482b788ef1af9d01a6f97a0dab67a3350
ChunniLal <chunnilal1103@gmail.com>
2024-04-09 01:08:12 +0530

    getting user data

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -10,7 +10,8 @@ const home = async(req, res) => {
 
 const register = async(req, res) => {
     try {
-        res.status(200).send("Hello this is register page from controllers");
+        console.log(req.body);
+        res.status(200).json({msg : req.body});
     } catch (error) {
         console.log(error);
     }
