972a2d491c31fc328bccd31350f392f2069bd2aa
ChunniLal <chunnilal1103@gmail.com>
2024-04-09 10:35:35 +0530

    userSchema created

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const User = require('../models/user-model');
 
 const home = async(req, res) => {
     try {
@@ -10,8 +11,15 @@ const home = async(req, res) => {
 
 const register = async(req, res) => {
     try {
-        console.log(req.body);
-        res.status(200).json({msg : req.body});
+        const {username, email, phone, password} = req.body;
+
+        const userExist = await User.findOne({email : email});
+        if(userExist){
+            res.status(400).json({msg : "User exist already"});
+        }
+
+        const newUser = await User.create({username, email, phone, password});
+        res.status(200).json({msg : newUser});
     } catch (error) {
         console.log(error);
     }
