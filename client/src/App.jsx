b2528c65d35fb7b6b5a3f9ed763b3aa64fb4e4d2
ChunniLal <chunnilal1103@gmail.com>
2024-04-11 02:36:24 +0530

    Routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Service } from "./pages/Service";
+import { Register } from "./pages/Register";
+import { Login } from "./pages/Login";
+import { Contact } from "./pages/Contact";
 
 function App() {
 
   return (
     <>
-      <h1>Starting with frontend</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="Login" element={<Login />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,3 @@
+export const Register = () => {
+    return <h1>This is register page</h1>
+}
