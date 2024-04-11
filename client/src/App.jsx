86d115e66cf907c8e36d3b920c903b19abd45d09
ChunniLal <chunnilal1103@gmail.com>
2024-04-11 11:39:58 +0530

    Register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,19 @@ import { Service } from "./pages/Service";
 import { Register } from "./pages/Register";
 import { Login } from "./pages/Login";
 import { Contact } from "./pages/Contact";
+import { Navbar } from "./components/Navbar";
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
-          <Route path="Login" element={<Login />} />
+          <Route path="login" element={<Login />} />
           <Route path="/service" element={<Service />} />
           <Route path="/register" element={<Register />} />
         </Routes>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,3 +1,72 @@
+import { useState } from "react";
+
 export const Register = () => {
-    return <h1>This is register page</h1>
-}
+    const [user, setUser] = useState({
+        username : "",
+        email : "",
+        phone : "",
+        password : ""
+    });
+
+    const handleInput = (e) => {
+        console.log(e);
+        let name = e.target.name;
+        let val = e.target.value
+        setUser({
+            ...user,
+            [name] : val
+        })
+    }
+    const handleForm = (e) => {
+        e.preventDefault();
+        console.log(user);
+    }
+  return (
+    <>
+      <section>
+        <main>
+          <div className="section-registration">
+            <div className="container grid grid-two-cols">
+              <div className="registration-image">
+                <img src="/images/register.jpg" alt="" width="400" height="200"/>
+              </div>
+              <div className="registration-form">
+                <h1 className="main-heading"></h1>
+                <br />
+                <form onSubmit={handleForm}>
+                    <div>
+                        <label htmlFor="username">Username : </label>
+                        <input type="text" name="username" placeholder="username"
+                        id="username" required autoComplete="off" value={user.username}
+                        onChange={handleInput}/>
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email : </label>
+                        <input type="text" name="email" placeholder="email"
+                        id="email" required autoComplete="off" value={user.email}
+                        onChange={handleInput}/>
+                    </div>
+                    <div>
+                        <label htmlFor="phone">Phone : </label>
+                        <input type="number" name="phone" placeholder="phone"
+                        id="phone" required autoComplete="off" value={user.phone}
+                        onChange={handleInput}/>
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password : </label>
+                        <input type="password" name="password" placeholder="password"
+                        id="password" required autoComplete="off" value={user.password}
+                        onChange={handleInput}/>
+                    </div>
+
+                    <br />
+                    <button type="submit">Submit</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </main>
+      </section>
+    </>
+  );
+};
