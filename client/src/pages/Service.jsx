a986019a75440252a9a3c473177ffcf89d97c90c
ChunniLal <chunnilal1103@gmail.com>
2024-04-14 01:28:12 +0530

    Services data get backend to frontend

--- client/src/pages/Service.jsx
+++ client/src/pages/Service.jsx
@@ -1,3 +1,22 @@
+import { useAuth } from "../store/auth";
+
 export const Service = () => {
-    return <h1>This is service page</h1>
-}
+  const { services } = useAuth();
+  return (
+    <>
+    <div>
+      {services && services.map((currEle, index) => {
+        const { service, description, price, provider } = currEle;
+        return (
+          <div key={index} style={{display:"flex", width:"500px", height:"500px", border: '1px solid red', padding: "10px"}}>
+            <p>{service}</p>
+            <p>{description}</p>
+            <h3>{price}</h3>
+            <h2>{provider}</h2>
+          </div>
+        );
+      })}
+      </div>
+    </>
+  );
+};
--- client/src/store/auth.jsx
+++ client/src/store/auth.jsx
@@ -5,6 +5,7 @@ export const AuthContext = createContext();
 export const AuthProvider = ({children}) => {
     const [token, setToken] = useState(localStorage.getItem("token"));
     const [user, setUser] = useState("");
+    const [services, setServices] = useState("");
 
     const storeTokeninLS = (serverToken) => {
         return localStorage.setItem('token', serverToken);
@@ -37,12 +38,29 @@ export const AuthProvider = ({children}) => {
         }
     }
 
+    const getServices = async(req, res) => {
+        try {
+            const response = await fetch("http://localhost:5000/api/data/service", {
+                method : "GET",
+            })
+
+            if(response.ok){
+                const data = await response.json();
+                console.log(data);
+                setServices(data.response);
+            }
+        } catch (error) {
+            console.log("Services frontend error");
+        }
+    }
+
     useEffect(() => {
+        getServices(),
         userAuthentication()
     }, []);
 
     return (
-        <AuthContext.Provider value={{ storeTokeninLS, isLoggedIn, LogoutUser, user }}>
+        <AuthContext.Provider value={{ storeTokeninLS, isLoggedIn, LogoutUser, user, services }}>
             {children}
         </AuthContext.Provider>
     )
